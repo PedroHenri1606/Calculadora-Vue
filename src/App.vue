9e240401409726df5281234cf1f4245e577d692d
Pedro Henrique <pedrohenri1606@gmail.com>
2023-05-31 20:36:46 -0300

    Crição projeto vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <CalculadoraTeste msg="CALCULADORA - TESTE"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CalculadoraTeste from './components/CalculadoraTeste.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    CalculadoraTeste
   }
 }
 </script>
---
+++ src/components/CalculadoraTeste.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <table class="table table-hover">
+      <td colspan="4">0</td> 
+      <tr>
+        <td>1</td>
+        <td>2</td>
+        <td>3</td>
+        <td>+</td>
+      </tr>
+      <tr>
+        <td>4</td>
+        <td>5</td>
+        <td>6</td>
+        <td>-</td>
+      </tr>
+      <tr>
+        <td>7</td>
+        <td>8</td>
+        <td>9</td>
+        <td>*</td>
+      </tr>
+      <tr>
+        <td>C</td>
+        <td>0</td>
+        <td>X</td>
+        <td>/</td>
+      </tr>
+    </table>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CalculadoraTeste',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
